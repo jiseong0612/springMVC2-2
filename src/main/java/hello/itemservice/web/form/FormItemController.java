d346394126d72c2cc6bc372c51a33db7e8300ef4
jiseong0612 <hth5545@gmail.com>
2022-03-16 02:14:16 +0900

    타임리프 1개 체크박스 false 값 받기, crud 추가

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,13 +3,15 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,6 +41,7 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    	log.info("item.isOpen = {}", item.isOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -54,6 +57,7 @@ public class FormItemController {
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    	log.info("item.getOpen = {}", item.isOpen());
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
